0900eac4beb077cb8bfc9bbd152e51471b8aaba1
Alexander Zarutskiy <sashasolo23@gmail.com>
2024-01-18 15:00:44 +0000

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Alexander Zarutskiy on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
